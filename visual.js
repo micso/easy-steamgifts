989c14e64463c78bb42a07c9a5a0c74c5de5778d
Josh <compujosh@users.noreply.github.com>
2017-07-06 00:08:19 -0400

    Update visual.js to account for SG layout changes

--- visual.js
+++ visual.js
@@ -36,9 +36,9 @@ var vis = {
     },
     getGiveawayData: function (item) {
         var enableIncrement = true;
-        var image = $('a.global__image-outer-wrap--game-medium', item)
-        if (image.hasClass('global__image-outer-wrap--missing-image')) image = null;
-        else image = $('> div.global__image-inner-wrap', image).attr('style').replace('background-image:url(', '').replace(');', '');
+        var image = $('a.giveaway_image_thumbnail', item);
+		if ($('a.giveaway_image_thumbnail', item).length == 0) image = null;
+		else image.attr('style').replace('background-image:url(', '').replace(');', '');
 
         if ($('.giveaway__heading > a.giveaway__icon', item).length) {
             var appId = parseInt($('.giveaway__heading > a.giveaway__icon', item).attr('href').match(/app\/[0-9]+|sub\/[0-9]+/)[0].replace('app/', '').replace('sub/', ''));
@@ -114,7 +114,7 @@ var vis = {
                     $('.giveaway__columns', this).prepend('<a href="javascript:void(0);" class="esg-bl-link"><div><i class="fa fa-plus-square"/> <span>eSG Blacklist</span></div></a>');
                     $('.giveaway__columns', this).prepend('<div class="esg-win-chance">' + giveaway.chance.toFixed(3) + '%</div>');
                     if (!giveaway.joined && giveaway.contributor.achievable && sg.points >= giveaway.points) {
-                        $('.global__image-outer-wrap--game-medium', this).prepend('<div class="esg-join"><a href="javascript:void(0);" points="' + giveaway.points + '" joinHref="' + giveaway.href + '">Join</a></div>');
+                        $('.giveaway_image_thumbnail', this).prepend('<div class="esg-join"><a href="javascript:void(0);" points="' + giveaway.points + '" joinHref="' + giveaway.href + '">Join</a></div>');
                     }
 
                     if (sg.storage.hideDlc == true) {
